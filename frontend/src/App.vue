477ab23e77d957cd2be1b23b2f69bcbda334d22e
nabilahaffyda <nabilahaffryda88@gmail.com>
2022-03-15 20:14:26 +0700

    add header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Header />
+    <v-main class="pt-0">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,176 @@
+<template>
+  <div>
+    <v-toolbar flat outlined class="px-15">
+      <v-spacer />
+      <v-toolbar-title>
+        <v-btn :to="{ name: 'home' }" text>
+          <v-img src="@/assets/menawi.jpg" max-width="100px" />
+        </v-btn>
+      </v-toolbar-title>
+      <v-spacer />
+      <!-- <v-toolbar-items v-if="isLoggedIn"> -->
+      <!-- level -->
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :rounded="rounded"
+        :nudge-width="150"
+        offset-y
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">
+            <v-icon color="green">mdi-star-circle</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <!-- level avatar -->
+              <v-list-item-avatar>
+                <img
+                  src="https://cdn.vuetifyjs.com/images/john.jpg"
+                  alt="John"
+                />
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <strong>LEVEL</strong>
+                </v-list-item-title>
+                <v-list-item-subtitle
+                  >Naikkan level keahlianmu!</v-list-item-subtitle
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+
+      <!-- badge -->
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :rounded="rounded"
+        :nudge-width="150"
+        offset-y
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">
+            <v-icon color="yellow">mdi-trophy</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <img
+                  src="https://cdn.vuetifyjs.com/images/john.jpg"
+                  alt="John"
+                />
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <strong>Pencapaian</strong></v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-list-item link class="text-center">
+              <v-list-item-subtitle class="text-decoration-underline"
+                >Lihat pencapaian lainnya</v-list-item-subtitle
+              >
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+
+      <!-- account -->
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :rounded="rounded"
+        offset-y
+        :nudge-width="150"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">
+            <v-icon color="primary">mdi-account-circle</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <v-list-item-title> <strong>AKUN</strong></v-list-item-title>
+            </v-list-item>
+            <v-divider />
+            <v-list-item link>
+              <v-list-item-title>Profilmu</v-list-item-title>
+            </v-list-item>
+            <v-divider />
+            <v-list-item @click="logout()">
+              <v-list-item-title>Keluar</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+      <!-- logout -->
+      <v-dialog v-model="dialogLogout" max-width="350px" activator="item">
+        <v-card>
+          <v-card-title class="text"
+            >Are you sure you want to logout?</v-card-title
+          >
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+            <v-btn color="blue darken-1" text @click="logoutConfirm">OK</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- </v-toolbar-items> -->
+
+      <!-- <v-toolbar-items v-else>
+        <v-btn text @click="redirectLogin()"> Login </v-btn></v-toolbar-items
+      > -->
+      <v-spacer />
+    </v-toolbar>
+  </div>
+</template>
+
+<style scoped>
+.v-toolbar {
+  transition: 0.6s;
+}
+
+.expand {
+  height: 80px !important;
+  padding-top: 10px;
+}
+</style>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      dialogLogout: false,
+    };
+  },
+  methods: {
+    close() {
+      this.dialogLogout = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+    logout() {
+      this.dialogLogout = true;
+    },
+    // logoutConfirm() {
+    //   this.$store.dispatch("logOut");
+    //   this.$router.push("/login");
+    //   this.close();
+    // },
+  },
+};
+</script>
