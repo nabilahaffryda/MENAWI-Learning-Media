073c9f165d03975d2fc7d86aa789100e142fa7c2
nabilahaffyda <nabilahaffryda88@gmail.com>
2022-05-14 23:45:46 +0700

    remove vee-validate

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,22 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-import { ValidationObserver } from 'vee-validate'
-import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm';
-
-Vue.component('ValidationProvider', ValidationProvider);
-Vue.component('ValidationObserver', ValidationObserver);
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   router,
-  data() {
-    return {
-      snackbar: false,
-      snackbarMsg: ''
-    }
-  },
   render: h => h(App)
 }).$mount('#app')
