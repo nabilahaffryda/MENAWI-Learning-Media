07fe9d5b8499edf11ea29b0e1a7ca663127b4e18
nabilahaffyda <nabilahaffryda88@gmail.com>
2022-05-14 23:46:09 +0700

    add register api

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -5,104 +5,112 @@
         <v-card class="pa-8" flat>
           <div class="text-center" style="margin-bottom: 20px">
             <h2 class="indigo--text">Buat Profilmu</h2>
-            <!-- <v-avatar size="50" color="indigo lighten-4">
-              <v-icon size="30" color="indigo">mdi-account</v-icon>
-            </v-avatar> -->
           </div>
-          <ValidationObserver v-slot="{ submitLogin }">
-            <form @submit.prevent="submitLogin">
-              <v-card-text class="text-center">
-                <ValidationProvider
-                  name="name"
-                  rules="required"
-                  v-slot="{ errors }"
+          <form @submit.prevent="submitLogin">
+            <v-card-text class="text-center">
+              <div class="form-group">
+                <v-text-field
+                  filled
+                  rounded
+                  style="margin-bottom: 10px"
+                  hide-details="auto"
+                  dense
+                  id="name"
+                  class="form-control"
+                  label="Name"
+                  v-model="form.name"
+                  type="text"
                 >
-                  <v-text-field
-                    filled
-                    rounded
-                    style="margin-bottom: 10px"
-                    hide-details="auto"
-                    dense
-                    label="Nama"
-                    v-model="form.nama"
-                    type="text"
-                  >
-                  </v-text-field>
-                  <p class="text-center red--text">{{ errors[0] }}</p>
-                </ValidationProvider>
-                <ValidationProvider
-                  name="username"
-                  rules="required"
-                  v-slot="{ errors }"
+                </v-text-field>
+                <p class="text-center red--text" v-if="errors.name">
+                  {{ errors.name[0] }}
+                </p>
+              </div>
+              <div class="form-group">
+                <v-text-field
+                  filled
+                  rounded
+                  style="margin-bottom: 13px"
+                  hide-details="auto"
+                  dense
+                  id="username"
+                  class="form-control"
+                  label="Username"
+                  v-model="form.username"
+                  type="text"
                 >
-                  <v-text-field
-                    filled
-                    rounded
-                    style="margin-bottom: 13px"
-                    hide-details="auto"
-                    dense
-                    label="Username"
-                    v-model="form.username"
-                    type="text"
-                  >
-                  </v-text-field>
-                  <p class="text-center red--text">{{ errors[0] }}</p>
-                </ValidationProvider>
-
-                <ValidationProvider
-                  name="password"
-                  rules="required|max:12|min:8"
-                  v-slot="{ errors }"
-                >
-                  <v-text-field
-                    filled
-                    rounded
-                    dense
-                    style="margin-bottom: 30px"
-                    hide-details="auto"
-                    label="Password"
-                    v-model="form.password"
-                    type="password"
-                  >
-                  </v-text-field>
-                  <p class="text-center red--text">{{ errors[0] }}</p>
-                </ValidationProvider>
-
-                <v-btn
-                  type="submit"
-                  color="indigo"
-                  block
-                  style="margin-bottom: 15px"
+                </v-text-field>
+                <p class="text-center red--text" v-if="errors.username">
+                  {{ errors.username[0] }}
+                </p>
+              </div>
+              <div class="form-group">
+                <v-text-field
                   filled
                   rounded
                   dense
-                  @click="submit()"
+                  id="password"
+                  class="form-control"
+                  style="margin-bottom: 30px"
+                  hide-details="auto"
+                  label="Password"
+                  v-model="form.password"
+                  type="password"
                 >
-                  <span class="white--text px-8">DAFTAR</span>
-                </v-btn>
-                <p class="text-center">
-                  Sudah punya akun?
-                  <router-link to="login">Masuk</router-link>
+                </v-text-field>
+                <p class="text-center red--text" v-if="errors.password">
+                  {{ errors.password[0] }}
                 </p>
-              </v-card-text>
-            </form>
-          </ValidationObserver>
+              </div>
+              <v-btn
+                type="submit"
+                color="indigo"
+                block
+                style="margin-bottom: 15px"
+                filled
+                rounded
+                dense
+                @click.prevent="register"
+              >
+                <span class="white--text px-8">DAFTAR</span>
+              </v-btn>
+              <p class="text-center">
+                Sudah punya akun?
+                <router-link to="login">Masuk</router-link>
+              </p>
+            </v-card-text>
+          </form>
         </v-card>
       </v-col>
     </v-layout>
   </v-container>
 </template>
 <script>
+import User from "../apis/User";
 export default {
   name: "Register",
   data() {
     return {
       form: {
-        username: "",
         name: "",
+        username: "",
         password: "",
       },
+      errors: [],
     };
+  },
+  methods: {
+    register() {
+      User.register(this.form)
+        .then(() => {
+          this.$router.push({ name: "Login" });
+        })
+        .catch((error) => {
+          if (error.response.status === 422) {
+            this.errors = error.response.data.errors;
+          }
+        });
+    },
   },
 };
 </script>
