95d3aa45778bbc876f88240ab231d80cb38c0105
nabilahaffyda <nabilahaffryda88@gmail.com>
2022-05-18 14:04:22 +0700

    change route writing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,23 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthAPIController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'api', 'as' => 'api.', 'middleware' => ['auth:sanctum']], function () {
+    Route::post('register', [AuthAPIController::class, 'register']);
+    Route::post('login', [AuthAPIController::class, 'login']);
+    Route::post('logout', [AuthAPIController::class, 'logout']);
 });
-Route::post('/register', [AuthAPIController::class, 'register']);
-Route::post('/login', [AuthAPIController::class, 'login']);
-Route::post('/logout', [AuthAPIController::class, 'logout']);
