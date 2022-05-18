9ed18f70887434ce32090be6cbf64730c4680bb0
nabilahaffyda <nabilahaffryda88@gmail.com>
2022-05-18 22:27:44 +0700

    fix data type picture value

---
+++ backend/database/migrations/2022_05_06_101918_change_question_pict_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('quiz_theme_question', function (Blueprint $table) {
+            $table->string('question_pict')->unsigned()->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
