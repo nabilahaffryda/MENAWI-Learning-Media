960fc61588a2a1e2f88d1ac463a9481db2a4b398
nabilahaffyda <nabilahaffryda88@gmail.com>
2022-07-05 13:14:33 +0700

    add basic for profile

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,36 +1,57 @@
 <template>
   <v-main class="white">
     <v-container class="top">
-      <v-row >
+      <v-row>
         <v-col cols="12" md="6" class="d-block ml-auto mr-auto">
-          <v-sheet
-           height="50vh"
-            rounded="lg"
-            outlined
-            style="margin-top: 20px"
-          >
+          <v-sheet height="70vh" rounded="lg" outlined style="margin-top: 20px">
             <v-container fluid>
               <v-row class="d-block ml-auto mr-auto">
                 <v-col cols="12" class="d-block ml-auto mr-auto">
                   <v-row align="center" class="d-block ml-auto mr-auto">
-                    <v-col cols="12" sm="6" class="d-block ml-auto mr-auto">
+                    <v-col cols="12" sm="8" class="d-block ml-auto mr-auto">
                       <h1 style="margin-top: 30px" class="text-center">
-                        Profil
+                        Edit Profil
                       </h1>
                       <v-divider></v-divider>
-                        <h3 class="text-center" style="margin-top: 30px">
+                      <v-form ref="form">
+                        <v-text-field
+                          v-model="name"
+                          label="Nama" style="margin-top: 20px"
+                          required
+                        ></v-text-field>
+                        <v-text-field
+                          v-model="username" style="margin-top: 10px"
+                          label="Username"
+                          required
+                        ></v-text-field>
+                        <v-text-field
+                          v-model="password" style="margin-top: 10px" type="password"
+                          label="Password Baru"
+                        ></v-text-field>
+                        <v-btn
+                          color="success"
+                          class="mr-2 float-right" style="margin-top: 20px"
+                        >
+                          Simpan
+                        </v-btn>
+                      </v-form>
+                      <!-- <h3 class="text-center" style="margin-top: 30px">
                         Username:
                       </h3>
-                      <span class="d-block ml-auto mr-auto text-center" style="margin-top: 10px">{{
-                        user.username
-                      }}</span>
-                      
+                      <span
+                        class="d-block ml-auto mr-auto text-center"
+                        style="margin-top: 10px"
+                        >{{ user.username }}</span
+                      >
+
                       <h3 class="text-center" style="margin-top: 20px">
                         Jeneng:
                       </h3>
-                      <span class="d-block ml-auto mr-auto text-center" style="margin-top: 10px">{{
-                        user.name
-                      }}</span>
+                      <span
+                        class="d-block ml-auto mr-auto text-center"
+                        style="margin-top: 10px"
+                        >{{ user.name }}</span
+                      > -->
                     </v-col>
                   </v-row>
                 </v-col>
@@ -56,9 +77,9 @@ export default {
 </script>
 <style>
 @media only screen and (max-width: 600px) {
-/* For mobile phones: */
-    [class*="cols="] {
-        width: 100%;
-    }
+  /* For mobile phones: */
+  [class*="cols="] {
+    width: 100%;
+  }
 }
 </style>
