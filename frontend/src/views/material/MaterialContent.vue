e544fea40b88cdffecf9ab8fb63d031a29ec75a3
nabilahaffyda <nabilahaffryda88@gmail.com>
2022-07-08 14:05:19 +0700

    fix url for access image

--- frontend/src/views/material/MaterialContent.vue
+++ frontend/src/views/material/MaterialContent.vue
@@ -12,7 +12,7 @@
                 <v-row>
                   <v-col cols="12" md="6">
                     <v-img
-                      :src="`https://admin.menawi.com/storage/${currentDescription.desc_pict}`"
+                      :src="`https://admin.menawi.com/storage/app/public/${currentDescription.desc_pict}`"
                       height="300px" class="d-block ml-auto mr-auto"
                       width="300px"
                       style="margin-top: 10px"
